2aad2c58570d67e2eb9b74d17563c75957b87199
amandapeixoto <amandapeixoto.ap1@gmail.com>
2025-01-24 16:08:24 -0300

    SQL, projection, GET games by list

--- src/main/java/com/maps/dslist/services/GameService.java
+++ src/main/java/com/maps/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.maps.dslist.services;
 import com.maps.dslist.dto.GameDTO;
 import com.maps.dslist.dto.GameMinDTO;
 import com.maps.dslist.entities.Game;
+import com.maps.dslist.projections.GameMinProjection;
 import com.maps.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+
+    }    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
